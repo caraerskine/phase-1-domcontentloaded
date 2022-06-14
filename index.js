28faac639525544a5ce3780dceaf98e05e1c55b5
CaraErskine <caraerskine@gmail.com>
2022-06-14 17:48:49 -0400

    commit tree dc48b30acd6adc14bca37d6edbdb2314d5d0f46b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('After DOM Loaded')
+})
+    console.log('Before DOM Loads')
+
+    //
+    
+  let message = document.getElementById('message');
+  document.addEventListener("DOMContentLoaded", (e) => {
+  text.textContent = `This is really cool!`
+  })
